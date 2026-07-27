2499aa4794ab887d51d51a227b2d1e9c57a6f61c
DanDomSer <dandomser@gmail.com>
2026-07-27 19:18:03 +0800

    Updated RoleSelectorPage.tsx

--- src/modules/auth/RoleSelectorPage.tsx
+++ src/modules/auth/RoleSelectorPage.tsx
@@ -62,7 +62,7 @@ export function RoleSelectorPage() {
           </div>
           <div>
             <h1 className="text-white font-bold text-base leading-tight">{APP_NAME}</h1>
-            <p className="text-green-200 text-xs leading-tight">Barangay Resiliency Platform</p>
+            <p className="text-green-200 text-xs leading-tight">Barangay Resiliency and Empowerment</p>
           </div>
         </div>
 
